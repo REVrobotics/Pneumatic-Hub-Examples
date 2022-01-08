5d4c8d0403f1aed6351a01df0a639f1cd2fe92e7
Trevor Van Doren <trevor@revrobotics.com>
2022-01-07 18:03:48 -0600

    initial commit until wpi bugs are fixed

---
+++ Java/Get Current and Voltages/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  private static final int PH_CAN_ID = 1;
+  PneumaticHub m_ph = new PneumaticHub(PH_CAN_ID);
+  
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+}
